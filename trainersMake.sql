321df01fec8c6c0e4c593d1bc331df53b2cac374
Thomas Plano <planot@wit.edu>
2016-03-31 13:56:03 -0400

    trainer scripts

---
+++ trainersMake.sql
@@ -0,0 +1,6 @@
+insert into Trianers (Name, Age, NumberOfBadges, Hometown) values ("Tom Plano", 19, 8, "Reading");
+insert into Trianers (Name, Age, NumberOfBadges, Hometown) values ("Cliff Anderson", 20, 5, "Freeport");
+insert into Trianers (Name, Age, NumberOfBadges, Hometown) values ("Will Larence", 21, 0, "Freeport");
+insert into Trianers (Name, Age, NumberOfBadges, Hometown) values ("John Russo", 30, 8, "Pallete Town");
+insert into Trianers (Name, Age, NumberOfBadges, Hometown) values ("Prez Obammy", 48, 8, "Not the US");
+insert into Trianers (Name, Age, NumberOfBadges, Hometown) values ("Trumps Hair", 99, 0, "Hell");
