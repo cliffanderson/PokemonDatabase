16cf20cf9b4c3349a8e0d254db5dfb68e1570d88
cliffanderson <cliffordmorgananderson@gmail.com>
2016-04-02 08:23:09 -0400

    made catches and trades create files

---
+++ catchesMake.sql
@@ -0,0 +1,8 @@
+#insert into Catches values(trainerID, pokemonID, regionID)
+#Cliff's team
+insert into Catches values(2, 255, 0);
+insert into Catches values(2, 136, 6);
+insert into Catches values(2, 146, 8);
+insert into Catches values(2, 244, 0);
+insert into Catches values(2, 155, 0);
+insert into Catches values(2, 250, 8);
